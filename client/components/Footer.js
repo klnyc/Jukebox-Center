f0b4114800de19a8c50ed28fde1db0d7a9da3184
Kenneth Lai <klnyc6@gmail.com>
2020-11-08 14:44:37 -0500

    changed delete button border radius

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -6,7 +6,6 @@ const Footer = () => {
             <a href="https://www.linkedin.com/in/kennethklai/">LinkedIn</a>
             <a href="https://github.com/klnyc">GitHub</a>
             <a href="https://www.instagram.com/kl.nyc">Instagram</a>
-            <a href="https://idestination.web.app/">iDestination</a>
         </div>
     )
 }
