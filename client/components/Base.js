618052d28cdbb1586320ee890daae8427e4b72eb
Kenneth Lai <klnyc6@gmail.com>
2020-11-08 14:15:09 -0500

    fixed footer and input limits

--- client/components/Base.js
+++ client/components/Base.js
@@ -8,7 +8,17 @@ class Base extends React.Component {
     }
 
     handleChange(event) {
-        this.setState({ [event.target.name]: event.target.value })
+        const inputName = event.target.name
+        const inputLength = event.target.value.length
+        if (inputName === 'quantity' || inputName === 'MM' || inputName === 'YY') {
+            if (inputLength <= 2) this.setState({ [event.target.name]: event.target.value })
+        } else if (inputName === 'CVC') {
+            if (inputLength <= 3) this.setState({ [event.target.name]: event.target.value })
+        } else if (inputName === 'CC') {
+            if (inputLength <= 16) this.setState({ [event.target.name]: event.target.value })
+        } else {
+            this.setState({ [event.target.name]: event.target.value })
+        }
     }
 
     formatPrice(price) {
