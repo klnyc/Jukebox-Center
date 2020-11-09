28264b7ad188d42894a35e5a7018568a74eefb56
Kenneth Lai <klnyc6@gmail.com>
2020-11-09 00:25:49 -0500

    removed links in footer

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,9 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="footer">
-            <a href="https://www.linkedin.com/in/kennethklai/">LinkedIn</a>
-            <a href="https://github.com/klnyc">GitHub</a>
-            <a href="https://www.instagram.com/kl.nyc">Instagram</a>
+            <div>Jukebox Center</div>
         </div>
     )
 }
