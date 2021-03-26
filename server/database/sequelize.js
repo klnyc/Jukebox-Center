4a64aaf79e75d47ec763b09049e1b76da4c88fc9
Kenneth Lai <kennethlai.ny@gmail.com>
2021-03-26 13:39:47 -0400

    added ssl heroku

--- server/database/sequelize.js
+++ server/database/sequelize.js
@@ -1,4 +1,26 @@
+let config
+
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    operatorsAliases: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+} else {
+  config = {
+    logging: false
+  }
+}
+
 const Sequelize = require('sequelize')
-const database = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/jukeboxcenter?ssl=true', { logging: false })
+const database = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/jukeboxcenter', config)
 
 module.exports = database
